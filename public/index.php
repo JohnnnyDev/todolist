a6679e1a1876b5dd7afe80695f0bbca8d063eeee
JohnnnyDev <125190963+JohnnnyDev@users.noreply.github.com>
2023-04-08 09:03:02 +0800

    Optimizing directory
    Optimizing directory of project

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+//require "../src/user.php";
+// require __DIR__. "/../vendor/autoload.php";
+
+// use todolist\user;
+// $newuser = new user();
+// $newuser->Msg();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8"> 
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Todolist</title>
+   <!-- css reset -->
+   <link rel="stylesheet" href="../assets/css/normalize.css">
+   <!-- bulma css -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+   <!-- jquery -->
+   <script
+   src="https://code.jquery.com/jquery-3.6.4.js"
+   integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
+   crossorigin="anonymous"></script>
+</head>
+<body>
+   <div class="container">
+      
+   </div>
+
+
+   
+</body>
+</html> 
---
+++ src/user.php
@@ -0,0 +1,11 @@
+<?php
+namespace todolist;
+
+class user{
+   public function Msg(){
+      echo "Hello world";
+   }
+}
+
+
+?>
